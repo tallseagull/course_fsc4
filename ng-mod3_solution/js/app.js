b2528b3131ef693a407b07544cb0d15b2147c90c
tallseagull <tsegalov@gmail.com>
2021-12-07 18:06:04 +0200

    Week 3 solution

---
+++ ng-mod3_solution/js/app.js
@@ -0,0 +1,69 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItems);
+
+function FoundItems() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      found: '<',
+      onRemove: '&'
+    },
+  };
+
+  return ddo;
+}
+
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var narrowIt = this;
+
+  // Initialize to having values in our list:
+  narrowIt.found = [];
+  narrowIt.search = "";
+  narrowIt.menuSearchService = MenuSearchService;
+
+  narrowIt.updateItems = function () {
+    narrowIt.menuSearchService.getMatchedMenuItems(narrowIt.search)
+        .then(function (foundItems) {
+          narrowIt.found = foundItems;
+        });
+  };
+
+  narrowIt.remove = function (idx) {
+    narrowIt.found.splice(idx, 1);
+  };
+}
+
+MenuSearchService.$inject = ['$http']
+function MenuSearchService($http) {
+  var service = this;
+
+  service.getMatchedMenuItems = function (searchTerm) {
+    return $http({
+      method: "GET",
+      url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+    }).then(function (result) {
+      // process result and only keep items that match
+      var foundItems = [];
+      var menuItems = result.data.menu_items;
+
+      console.log("Searching for " + searchTerm);
+      for (let i=0; i < menuItems.length ; i++) {
+        if (menuItems[i].name.includes(searchTerm)) {
+          foundItems.push(menuItems[i]);
+        }
+      }
+      // return processed items
+      return foundItems;
+    });
+  };
+
+}
+
+})();
